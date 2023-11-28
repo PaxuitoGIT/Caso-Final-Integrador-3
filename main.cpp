50f91d8bf86df14e5e97498d7d1c544266bb8688
Binhao <146821720+PaxuitoGIT@users.noreply.github.com>
2023-11-28 11:15:24 +0000

    creacion variant

--- main.cpp
+++ main.cpp
@@ -4,3 +4,12 @@
 #include <json11/json11.hpp>
 
 using namespace std;
+
+class Variant {
+public:
+    using VariantType = std::variant<std::string, int, double, bool, std::vector<Variant>>;
+    VariantType value;
+
+    Variant() = default;
+    Variant(VariantType value) : value(value) {}
+}
